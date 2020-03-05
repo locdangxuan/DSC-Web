bc212702932d97d150ee033fa427345a452b74ad
Dinh Hung <vodinhhung2204@gmail.com>
2020-03-05 13:49:08 +0700

    update coming-soon template

---
+++ components/Coming-soon/OrbitalStation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="orbital-station-img" />
+</template>
+
+<script>
+export default {
+  name: "OrbitalStation"
+}
+</script>
+
+<style lang="scss" scoped>
+.orbital-station-img {
+  background: url(~assets/img/coming-soon/Orbital_Station.svg);
+  background-size: cover;
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  top: 10%;
+}
+</style>
---
+++ templates/coming-soon.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <OrbitalStation />
+    <SpaceShuttle />
+    <Content />
+  </div>
+</template>
+<script>
+export default {
+  name: "ContactForm",
+  components: {
+    OrbitalStation: () => import("~/components/Coming-soon/OrbitalStation.vue"),
+    SpaceShuttle: () => import("~/components/Coming-soon/SpaceShuttle.vue"),
+    Content: () => import("~/components/Coming-soon/Title.vue")
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+  position: relative;
+}
+@media (max-width: 992px) {
+  .container {
+    height: 88vh;
+    position: relative;
+  }
+}
+</style>
